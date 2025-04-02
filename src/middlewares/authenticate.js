4941726dfe6daa41c133ffe61792aa0fa5c7fba5
Viktor_Baratejj <Viktor_baratejj@ukr.net>
2025-04-02 15:13:42 +0300

    add fix User

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import createHttpError from 'http-errors';
-import User from '../models/user.js';
+import User from '../models/User.js';
 import Session from '../models/session.js';
 
 const authenticate = async (req, res, next) => {
