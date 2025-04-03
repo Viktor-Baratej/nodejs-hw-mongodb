97d2bb0becc426f5fdbd7581fe2bdfb6d822e8ea
Viktor_Baratejj <Viktor_baratejj@ukr.net>
2025-04-04 01:10:07 +0300

    add fixed autorization

--- src/auth/googleStrategy.js
+++ src/auth/googleStrategy.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
       clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: 'https://nodejs-hw-mongodb-7-gti2.onrender.com/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
